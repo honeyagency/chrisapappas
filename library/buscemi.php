41cb3468a77575d017821a5f7d9fb156ff4370ef
joshre <joshre@me.com>
2017-11-14 15:59:51 -0800

    RM that

--- library/buscemi.php
+++ library/buscemi.php
@@ -37,7 +37,7 @@ function jquery_enqueue()
 {
     wp_dequeue_script('jquery');
     wp_deregister_script('jquery');
-    wp_register_script('jquery', "http" . ($_SERVER['SERVER_PORT'] == 443 ? "s" : "") . "://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.slim.min.js", false, '5', true);
+    wp_register_script('jquery', "http" . ($_SERVER['SERVER_PORT'] == 443 ? "s" : "") . "://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.slim.min.js", false, false, true);
 
 }
 
@@ -50,8 +50,7 @@ function buscemi_scripts()
     wp_enqueue_script('jquery');
     wp_register_script('picturefill', get_template_directory_uri() . '/app/vendors/picturefill.min.js', array('jquery'), false, true);
     wp_enqueue_script('picturefill');
-
-    wp_enqueue_style('buscemi_style', get_template_directory_uri() . '/app/main.min.css', null, '5', null);
+    wp_enqueue_style('buscemi_style', get_template_directory_uri() . '/app/main.min.css', null, null, null);
     wp_enqueue_script('buscemi_script', get_template_directory_uri() . '/app/app.min.js', array('jquery'), null, true);
 }
 add_action('wp_enqueue_scripts', 'buscemi_scripts');
