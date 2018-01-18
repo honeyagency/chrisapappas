01758db20d0c8ef0c768097495a149abe0090aa3
joshre <joshre@me.com>
2018-01-18 09:30:50 -0800

    RM that rough style flash

--- library/buscemi.php
+++ library/buscemi.php
@@ -51,7 +51,7 @@ function buscemi_scripts()
     wp_enqueue_script('jquery');
     wp_register_script('picturefill', get_template_directory_uri() . '/app/vendors/picturefill.min.js', array('jquery'), false, true);
     wp_enqueue_script('picturefill');
-    // wp_enqueue_style('buscemi_style', get_template_directory_uri() . '/app/main.min.css', null, null, null);
+    wp_enqueue_style('buscemi_style', get_template_directory_uri() . '/app/main.min.css', null, null, null);
     wp_enqueue_script('buscemi_script', get_template_directory_uri() . '/app/app.min.js', array('jquery'), null, true);
 }
 add_action('wp_enqueue_scripts', 'buscemi_scripts');
